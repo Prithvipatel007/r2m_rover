bb09deaf69400f6d4993ace3de4b9c02bd2a5123
daisy <siyi.dai@outlook.com>
2019-03-27 16:57:46 +0100

    simulation added

---
+++ r2m_simulation/build/curiosity_mars_rover_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/daisy/Desktop/R2M/r2m_ws/src/curiosity_mars_rover_description
+# Build directory: /home/daisy/Desktop/R2M/r2m_ws/build/curiosity_mars_rover_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
