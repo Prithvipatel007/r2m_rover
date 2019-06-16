a4200c0099824f8fcbaf38a284b3622af77a4b57
Prithvipatel007 <prithvipatel007@gmail.com>
2019-06-16 12:09:11 +0200

    r2m_ncs has been added

---
+++ r2m_ncs/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/iki/r2m_ncs/src
+# Build directory: /home/iki/r2m_ncs/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ r2m_ncs/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/iki/r2m_ncs/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/iki/simulation_ws/devel;/home/iki/rover_prototype/devel;/home/iki/catkin_arm_wc/devel;/home/iki/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/iki/simulation_ws/devel;/home/iki/rover_prototype/devel;/home/iki/catkin_arm_wc/devel;/home/iki/catkin_ws/devel;/opt/ros/kinetic" != "" else []
